8fc8387249be4380f17e9c6464a9775e84ef8671
Lavardur <antonben44@gmail.com>
2024-10-29 17:03:39 +0000

    verk 9 virkni komin

--- Verkefni-9/src/index.js
+++ Verkefni-9/src/index.js
@@ -45,12 +45,21 @@ const locations = [
   },
 ];
 
+let resultsElement;
+
 /**
  * Hreinsar fyrri niðurstöður, passar að niðurstöður séu birtar og birtir element.
  * @param {Element} element
  */
 function renderIntoResultsContent(element) {
-  // TODO útfæra
+  // Hreinsa fyrri niðurstöður
+  resultsElement.innerHTML = '';
+
+  // Gera niðurstöður sýnilegar
+  resultsElement.classList.remove('hidden');
+
+  // Bæta nýjum niðurstöðum við
+  resultsElement.appendChild(element);
 }
 
 /**
@@ -59,7 +68,45 @@ function renderIntoResultsContent(element) {
  * @param {Array<import('./lib/weather.js').Forecast>} results
  */
 function renderResults(location, results) {
-  // TODO útfæra
+  // Búa til niðurstöðuelement
+  const resultsContainer = document.createElement('div');
+
+  // Bæta við titli fyrir staðsetningu
+  const heading = document.createElement('h2');
+  heading.appendChild(document.createTextNode(`Veðurspá fyrir ${location.title}`));
+  resultsContainer.appendChild(heading);
+
+  // Búa til lista af veðurspám
+  const forecastList = document.createElement('ul');
+  forecastList.classList.add('forecasts');
+
+  for (const result of results) {
+    const item = document.createElement('li');
+    item.classList.add('forecast');
+
+    const date = document.createElement('div');
+    date.classList.add('forecast__date');
+    date.appendChild(document.createTextNode(result.time));
+
+    const description = document.createElement('div');
+    description.classList.add('forecast__description');
+    description.appendChild(document.createTextNode(result.description));
+
+    const temp = document.createElement('div');
+    temp.classList.add('forecast__temp');
+    temp.appendChild(document.createTextNode(`${result.temperature}°C`));
+
+    item.appendChild(date);
+    item.appendChild(description);
+    item.appendChild(temp);
+
+    forecastList.appendChild(item);
+  }
+
+  resultsContainer.appendChild(forecastList);
+
+  // Birtum niðurstöður
+  renderIntoResultsContent(resultsContainer);
 }
 
 /**
@@ -67,7 +114,11 @@ function renderResults(location, results) {
  * @param {Error} error
  */
 function renderError(error) {
-  // TODO útfæra
+  const errorElement = document.createElement('p');
+  errorElement.classList.add('error');
+  errorElement.appendChild(document.createTextNode(`Villa kom upp: ${error.message}`));
+
+  renderIntoResultsContent(errorElement);
 }
 
 /**
@@ -75,7 +126,10 @@ function renderError(error) {
  */
 function renderLoading() {
   console.log('render loading');
-  // TODO útfæra
+  const loadingElement = document.createElement('p');
+  loadingElement.appendChild(document.createTextNode('Hleð veðri...'));
+
+  renderIntoResultsContent(loadingElement);
 }
 
 /**
@@ -89,19 +143,52 @@ async function onSearch(location) {
   // Birta loading state
   renderLoading();
 
-  const results = await weatherSearch(location.lat, location.lng);
+  try {
+    const results = await weatherSearch(location.lat, location.lng);
+    console.log(results);
 
-  console.log(results);
-  // TODO útfæra
-  // Hér ætti að birta og taka tillit til mismunandi staða meðan leitað er.
+    // Birta niðurstöður
+    renderResults(location, results);
+  } catch (error) {
+    // Birta villu
+    renderError(error);
+  }
 }
 
 /**
  * Framkvæmir leit að veðri fyrir núverandi staðsetningu.
  * Biður notanda um leyfi gegnum vafra.
  */
-async function onSearchMyLocation() {
-  // TODO útfæra
+function onSearchMyLocation() {
+  if (navigator.geolocation) {
+    renderLoading();
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+
+        try {
+          const results = await weatherSearch(lat, lng);
+
+          const location = {
+            title: 'Núverandi staðsetningu',
+            lat,
+            lng,
+          };
+
+          renderResults(location, results);
+        } catch (error) {
+          renderError(error);
+        }
+      },
+      () => {
+        renderError(new Error('Ekki tókst að sækja staðsetningu'));
+      },
+    );
+  } else {
+    renderError(new Error('Vafrinn styður ekki geolocation'));
+  }
 }
 
 /**
@@ -122,15 +209,6 @@ function renderLocationButton(locationTitle, onSearch) {
     ),
   );
 
-  /* Til smanburðar við el fallið ef við myndum nota DOM aðgerðir
-  const locationElement = document.createElement('li');
-  locationElement.classList.add('locations__location');
-  const locationButton = document.createElement('button');
-  locationButton.appendChild(document.createTextNode(locationTitle));
-  locationButton.addEventListener('click', onSearch);
-  locationElement.appendChild(locationButton);
-  */
-
   return locationElement;
 }
 
@@ -149,12 +227,16 @@ function render(container, locations, onSearch, onSearchMyLocation) {
   // Búum til <header> með beinum DOM aðgerðum
   const headerElement = document.createElement('header');
   const heading = document.createElement('h1');
-  heading.appendChild(document.createTextNode('<fyrirsögn>'));
+  heading.appendChild(document.createTextNode('Veðurspá'));
   headerElement.appendChild(heading);
   parentElement.appendChild(headerElement);
 
-  // TODO útfæra inngangstexta
-  // Búa til <div class="loctions">
+  // Útfæra inngangstexta
+  const introText = document.createElement('p');
+  introText.appendChild(document.createTextNode('Veldu staðsetningu til að sjá veðurspá.'));
+  parentElement.appendChild(introText);
+
+  // Búa til <div class="locations">
   const locationsElement = document.createElement('div');
   locationsElement.classList.add('locations');
 
@@ -162,10 +244,17 @@ function render(container, locations, onSearch, onSearchMyLocation) {
   const locationsListElement = document.createElement('ul');
   locationsListElement.classList.add('locations__list');
 
-  // <div class="loctions"><ul class="locations__list"></ul></div>
+  // Setja locationsListElement inn í locationsElement
   locationsElement.appendChild(locationsListElement);
 
-  // <div class="loctions"><ul class="locations__list"><li><li><li></ul></div>
+  // --- Add "Use current location" button first ---
+  const myLocationButtonElement = renderLocationButton('Nota núverandi staðsetningu', () => {
+    console.log('Using my location');
+    onSearchMyLocation();
+  });
+  locationsListElement.appendChild(myLocationButtonElement);
+
+  // --- Add buttons for each predefined location ---
   for (const location of locations) {
     const liButtonElement = renderLocationButton(location.title, () => {
       console.log('Halló!!', location);
@@ -174,12 +263,18 @@ function render(container, locations, onSearch, onSearchMyLocation) {
     locationsListElement.appendChild(liButtonElement);
   }
 
+  // Setja locationsElement inn í parentElement
   parentElement.appendChild(locationsElement);
 
-  // TODO útfæra niðurstöðu element
+  // Útfæra niðurstöðu element
+  resultsElement = document.createElement('div');
+  resultsElement.classList.add('results');
+  resultsElement.classList.add('hidden'); // Felum í byrjun
+  parentElement.appendChild(resultsElement);
 
   container.appendChild(parentElement);
 }
+
 
 // Þetta fall býr til grunnviðmót og setur það í `document.body`
 render(document.body, locations, onSearch, onSearchMyLocation);
--- Verkefni-9/src/lib/weather.js
+++ Verkefni-9/src/lib/weather.js
@@ -9,7 +9,39 @@ async function sleep(ms) {
  * @property {string} time
  * @property {number} temperature
  * @property {number} precipitation
+ * @property {string} description
  */
+
+const weatherCodeDescriptions = {
+  0: 'Clear sky',
+  1: 'Mainly clear',
+  2: 'Partly cloudy',
+  3: 'Overcast',
+  45: 'Fog',
+  48: 'Depositing rime fog',
+  51: 'Light drizzle',
+  53: 'Moderate drizzle',
+  55: 'Dense drizzle',
+  56: 'Light freezing drizzle',
+  57: 'Dense freezing drizzle',
+  61: 'Slight rain',
+  63: 'Moderate rain',
+  65: 'Heavy rain',
+  66: 'Light freezing rain',
+  67: 'Heavy freezing rain',
+  71: 'Slight snowfall',
+  73: 'Moderate snowfall',
+  75: 'Heavy snowfall',
+  77: 'Snow grains',
+  80: 'Slight rain showers',
+  81: 'Moderate rain showers',
+  82: 'Violent rain showers',
+  85: 'Slight snow showers',
+  86: 'Heavy snow showers',
+  95: 'Thunderstorm',
+  96: 'Thunderstorm with slight hail',
+  99: 'Thunderstorm with heavy hail',
+};
 
 /**
  * Tekur við gögnum frá Open Meteo og skilar fylki af spám í formi Forecast.
@@ -17,7 +49,25 @@ async function sleep(ms) {
  * @returns {Array<Forecast>}
  */
 function parseResponse(data) {
-  return data;
+  // Extract hourly data from the API response
+  const times = data.hourly.time;
+  const temperatures = data.hourly.temperature_2m;
+  const precipitations = data.hourly.precipitation;
+  const weatherCodes = data.hourly.weathercode;
+
+  const forecasts = [];
+
+  // Combine the data into an array of Forecast objects
+  for (let i = 0; i < times.length; i++) {
+    forecasts.push({
+      time: times[i],
+      temperature: temperatures[i],
+      precipitation: precipitations[i],
+      description: weatherCodeDescriptions[weatherCodes[i]] || 'Unknown',
+    });
+  }
+
+  return forecasts;
 }
 
 /**
@@ -28,16 +78,14 @@ function parseResponse(data) {
  */
 export async function weatherSearch(lat, lng) {
   await sleep(1000);
-  // Querystring sem við viljum senda með leit
-  // latitude={lat}&longitude={lng}}&hourly=temperature_2m,precipitation&timezone=GMT&forecast_days=1
 
-  // TODO útfæra
-  // Hér ætti að nota URL og URLSearchParams
+  // Build the URL with query parameters
   const url = new URL(API_URL);
   const querystring = new URLSearchParams({
     latitude: lat.toString(),
     longitude: lng.toString(),
-    hourly: 'temperature_2m,precipitation',
+    hourly: 'temperature_2m,precipitation,weathercode',
+    timezone: 'GMT',
     forecast_days: '1',
   });
   url.search = querystring.toString();
@@ -46,7 +94,8 @@ export async function weatherSearch(lat, lng) {
 
   if (response.ok) {
     const data = await response.json();
-
     return parseResponse(data);
+  } else {
+    throw new Error('Ekki tókst að sækja veðurspá');
   }
 }
